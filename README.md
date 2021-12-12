86b38926ba1ce5b915f534ce1f1b2d1f558a0a56
Václav Hanzlík <hanzlikv@gmail.com>
2021-12-13 00:23:01 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # One Catalogue
- One Catalogue
+První pokus o zpracování zadání 
+Test for One Catalogue Team @ Heureka
+We want our test to be close to what you would actually be doing if accepted. Therefore we would like you to create a small Catalogue service. 
+Our goal is to have the assignment easy enough to be doable within 6-8 hours, yet difficult enough so that you can showcase what you are capable of. If you think we aren’t meeting either of those goals, please let us know!
+Assignment
+Create a service in Python/Golang. Publish its source code on GitHub or GitLab.
+
+Minimum requirements:
+Consumes messages from Kafka (details below)
+Stores data based on those messages (database of your choice) #- zvolil jsem MongoDB, pro správnou funkci je potřeba nainstalovat
+Not all data are present in the messages, some need to be fetched from an existing API
+When a product (see below) is matched with another, check how many parameters they have in common and how many differ, store that information 
+Test your service
+Optional:
+Deploy the service, or make it as close to deployable as you can
+Utilize CI/CD tools (Travis CI, Circle CI, GitLab CI...) as you see fit
+Manage your python/golang dependencies the way you think is the best 
+Make it easy to run your service locally
+Expose prometheus metrics
+Kafka
+Bootstrap Server: pkc-4ygn6.europe-west3.gcp.confluent.cloud:9092
+Security Protocol: SASL_SSL
+Username: KTIDDULUPUETVKOR
+Password: h0EsaIyNsFKrejrLmVsJZwOahA+2wgXBbSdHO/jJNyrEJ0p19LV+bdYeD2XR3gRU
+SASL mechanism: PLAIN
+Topic: catalogue_source
+Data in Kafka
+Every message is json and has the following format: {metadata: {type:”...”}, payload: {}}
+There is no uniqueness guarantee, some messages might be present multiple times.
+There are following types:
+offer
+Belongs to a category
+Has ID
+Has a name
+Has a description
+Has multiple parameters (nested inside)
+Same ‘product’ can be found in other eshops, not included in data, information can be found through API
+category
+Has a name
+Can have a parent category
+API
+Docker image, exposing the API at port 5000: <>
+To run, use docker run -it -p 5000:5000 heurekaoc/testday-api:latest 
+Required headers: {“Auth”: “827e8e1a-119c-48e2-af1c-cef81f933a5a”}
+GET /offer-matches/<offer-id>
+Returns a json with: {“matching_offers”: [“offer-1-id”, “offer-2-i
+
+
+
+
+
+
+
--- process_data.py
+++ process_data.py
@@ -10,33 +10,33 @@ AUTH = '827e8e1a-119c-48e2-af1c-cef81f933a5a'
 SASL_PASS = 'h0EsaIyNsFKrejrLmVsJZwOahA+2wgXBbSdHO/jJNyrEJ0p19LV+bdYeD2XR3gRU'
 SASL_USER = 'KTIDDULUPUETVKOR'
 BOOTSTRAP = ['pkc-4ygn6.europe-west3.gcp.confluent.cloud:9092']
+
 from json import loads
 from kafka import KafkaConsumer
 import requests
 from pymongo import MongoClient
 
 
-
 class Mongodatabase ():
     def __init__(self, address: str = 'localhost:27017'):
         self.client = MongoClient('localhost:27017')
         self.db = self.client.catalogue
-    def insert_new_product (self, message):
+
+    def insert_new_product(self, message):
         self.db.products.insert_one(message)
+
     def find_message_by_id(self, message_id):
         return self.db.products.find_one({'payload.id': message_id})
-       
-# import logging
-# logging.basicConfig(level=logging.DEBUG)
+
 
 def start_process():
     Mongo = Mongodatabase()
     consumer = KafkaConsumer('catalogue_source',
                              bootstrap_servers=BOOTSTRAP,
-                             security_protocol = 'SASL_SSL',
-                             sasl_mechanism = 'PLAIN',
-                             sasl_plain_username = SASL_USER,
-                             sasl_plain_password = SASL_PASS,
+                             security_protocol='SASL_SSL',
+                             sasl_mechanism='PLAIN',
+                             sasl_plain_username=SASL_USER,
+                             sasl_plain_password=SASL_PASS,
                              auto_offset_reset='earliest',
                              enable_auto_commit=True,
                              value_deserializer=lambda x: loads(x.decode('utf-8')))
@@ -47,12 +47,12 @@ def start_process():
             offer_id = message.get('payload').get('id')
             if offer_id is not None:
                 url = f'http://127.0.0.1:5000/offer-matches/{offer_id}'
-                r = requests.get(url =url,  headers={'auth' : AUTH} )
+                r = requests.get(url=url,  headers={'auth': AUTH})
                 if r:
                     matching_offers = r.json().get('matching_offers')
-                    message_params =  message.get('payload').get('parameters')
-                    message['payload']['matching_offers']=[]
-                    if len (matching_offers)>0:
+                    message_params = message.get('payload').get('parameters')
+                    message['payload']['matching_offers'] = []
+                    if len(matching_offers) > 0:
                         for matching_offer in matching_offers:
                             joint_params = []
                             differen_params = []
@@ -64,9 +64,11 @@ def start_process():
                                     joint_params.append(key)
                                 else:
                                     differen_params.append(key)
-                            message['payload']['matching_offers'].append({'m_offer_id':m_offer_id,
-                                                                          'joint_params':joint_params,
-                                                                          'differen_params':differen_params}) 
+                            message['payload']['matching_offers'].append({'m_offer_id': m_offer_id,
+                                                                          'joint_params': joint_params,
+                                                                          'differen_params': differen_params})
         Mongo.insert_new_product(message)
-        
+
 start_process()
+
+
